9a0c1901385eb4de0382519241ccd016051587a4
gustavodocarmokamitani <gustavodocarmokamitani@gmail.com>
2022-03-03 13:22:36 -0300

    build(:tada:): begin a project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const menuItems = document.querySelectorAll('.navbar a[href^="#"] ')
+
+menuItems.forEach(items => {
+    items.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault()
+    const element = event.target    
+    const id = element.getAttribute("href")
+    const to = document.querySelector(id).offsetTop
+
+    window.scroll({
+        top: to - 100,
+        behavior: "smooth"
+    })
+}
